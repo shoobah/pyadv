a14e35cf9b932246f9c8f4e50b99b5db85ae610b
shoobah <shoobah@gmail.com>
2015-05-18 15:42:25 +0200

    Module 3, decorators

---
+++ module-3/escape_unicode.py
@@ -0,0 +1,9 @@
+def escape_unicode(f):
+    def wrap(*args, **kwargs):
+        x = f(*args, **kwargs)
+        return ascii(x)
+    return wrap
+
+@escape_unicode
+def northern_city():
+    return 'Töreboda'
