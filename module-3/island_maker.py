339b6ef67d2be55ea86cc5c5e0964a260079bac8
shoobah <shoobah@gmail.com>
2015-05-18 15:49:51 +0200

    Module 3, class decorators

--- module-3/island_maker.py
+++ module-3/island_maker.py
@@ -22,3 +22,10 @@ tracer = Trace()
 def swedish_island_maker(name):
     return name + 'ön'
 
+class IslandMaker:
+    def __init__(self, suffix):
+        self.suffix = suffix
+
+    @tracer
+    def make_island(self, name):
+        return name + self.suffix
